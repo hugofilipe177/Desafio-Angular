40696c68ebf09f310ed0a7904c9da2c02dcbbbc6
Hugo Filipe <kylon9867@gmail.com>
2025-04-22 15:33:38 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { CarroComponent } from './componentes/carro/carro.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, CarroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/componentes/carro/carro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carro',
+  imports: [],
+  templateUrl: './carro.component.html',
+  styleUrl: './carro.component.css'
+})
+export class CarroComponent {
+
+}
