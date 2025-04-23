5cd90106da2f240cb50fc36ea7dca1ab2c935112
Hugo Filipe <kylon9867@gmail.com>
2025-04-23 16:51:05 -0300

    api

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+}
+const nomeInput = document.getElementById('nome') as HTMLInputElement;
+var nome: string = nomeInput? nomeInput.value :''; 
+
+var senha: string | null = (document.getElementById('senha') as HTMLInputElement).value;
+
+if (nome !== "" && senha !== "") {
+  
 }
