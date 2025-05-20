ace0e656d86e4466ca64979a729de4ce6d09bdb4
Hugo Filipe <kylon9867@gmail.com>
2025-05-20 15:24:55 -0300

    Final

--- src/app/componentes/servico-back/servico-back.service.ts
+++ src/app/componentes/servico-back/servico-back.service.ts
@@ -21,21 +21,21 @@ logado(): boolean {
   return !!(userLocal || userSession);
 }
 api_login(nome: string | null, senha: string | null): Observable<any> {  
-  return this.http.post('http://localhost:3001/login', { nome, senha });
+  return this.http.post('https://api-czja.onrender.com/login', { nome, senha });
 };
 
 api_Carro(): Observable<any>{
-  return this.http.get('http://localhost:3001/vehicles');
+  return this.http.get('https://api-czja.onrender.com/vehicles');
   
 };
 api_vin(vin:string):Observable<vinVeiculos>{
-  return this.http.post<vinVeiculos>('http://localhost:3001/vehicleData', {vin} );
+  return this.http.post<vinVeiculos>('https://api-czja.onrender.com/vehicleData', {vin} );
 }
 api_carousel(): Observable<any>{
-  return this.http.get('http://localhost:3001/carousel');
+  return this.http.get('https://api-czja.onrender.com/carousel');
 }
 api_comparador(): Observable<any>{
-  return this.http.get('http://localhost:3001/vehicleSpecs');
+  return this.http.get('https://api-czja.onrender.com/vehicleSpecs');
 }
 }
 
